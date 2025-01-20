628eb0fb5d4ca9c2a2f6191bb57b545a6e0bb23b
Fred <fred@moller.cloud>
2025-01-20 12:33:05 +0200

    cursors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,3 +13,14 @@ nvm use default --silent
 
 # Set up Flutter
 set PATH /home/fred/.flutter/flutter/bin $PATH
+
+# pnpm
+set -gx PNPM_HOME "/home/fred/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
