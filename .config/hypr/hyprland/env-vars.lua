b2b63b508000a02397f931df56b8b02e40c3c3e4
Fred <12495374+FredM7@users.noreply.github.com>
2026-08-25 17:28:02 +0200

    quickshell stuff

--- .config/hypr/hyprland/env-vars.lua
+++ .config/hypr/hyprland/env-vars.lua
@@ -6,3 +6,10 @@ hl.env("HYPRCURSOR_THEME", "Capitaine-Dark")
 hl.env("GSK_RENDERER", "ngl")
 hl.env("AMD_VULKAN_ICD", "RADV")
 hl.env("ELECTRON_OZONE_PLATFORM_HINT", "wayland")
+
+-- below 2 was added trying to get jellyfin to work, but it didn't help so far
+-- hl.env("QT_QPA_PLATFORM", "xcb")
+-- hl.env("LC_NUMERIC", "C")
+
+-- this was added for quickshell to force wayland
+hl.env("QT_QPA_PLATFORM", "wayland")
